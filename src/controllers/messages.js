874714dccd7c2f12bb79ffe7f0f7f3e3164b2150
rdmh12 <276881386+rdmh12@users.noreply.github.com>
2026-06-20 10:05:00 +0200

    Implement list of messages

--- src/controllers/messages.js
+++ src/controllers/messages.js
@@ -1,5 +1,20 @@
+const messages = [
+  {
+    text: "hello...",
+    user: "User 1",
+    created: new Date(),
+  },
+  {
+    text: "hello?",
+    user: "User 2",
+    created: new Date(),
+  },
+];
+
 export function getAll(_req, res) {
   res.render("index", {
     title: "Messages",
+    content: "messages-list",
+    messages: messages,
   });
 }
