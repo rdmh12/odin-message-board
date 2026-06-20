43cfffc2f4ad4a53a453a4f5c51dcb712c8872c8
rdmh12 <276881386+rdmh12@users.noreply.github.com>
2026-06-20 11:15:25 +0200

    Pre-process messages before rendering

--- src/controllers/messages.js
+++ src/controllers/messages.js
@@ -1,20 +1,35 @@
 const messages = [
   {
-    text: "hello...",
+    text: "Morbi ut felis vel felis accumsan volutpat. Donec aliquam ut arcu nec tincidunt. Nam id diam sed libero porta lacinia ac quis purus. Ut in sollicitudin arcu. Aliquam laoreet, nibh ut euismod maximus, est erat dictum odio, bibendum euismod metus turpis sit amet libero. Quisque iaculis tincidunt posuere. Mauris pretium rhoncus arcu nec ultrices. Cras rutrum volutpat tortor, id aliquet neque molestie sit amet. Vestibulum vel mi vitae arcu ultrices convallis ut nec leo.",
     user: "User 1",
     created: new Date(),
   },
   {
-    text: "hello?",
+    text: "Duis eget lectus massa. Nam dignissim nisi in maximus convallis. Vivamus quis lacinia ipsum, nec cursus dolor. Phasellus sagittis nisi lorem, sit amet iaculis odio tincidunt id. Proin nisi ipsum, dictum id elit sed, tristique suscipit tortor. Fusce sodales rutrum nibh id fermentum. Proin turpis sem, fringilla nec scelerisque sed, sodales vitae nisi.",
     user: "User 2",
     created: new Date(),
   },
 ];
 
-export function getAll(_req, res) {
+export function getAll(req, res) {
+  const languages = req.acceptsLanguages();
+  const language = languages.length > 0 ? languages[0] : "en-US";
+  const maxMessageSize = 50;
+
+  const processedMessages = messages
+    .toSorted((a, b) => b.created - a.created)
+    .map((message) => ({
+      user: message.user,
+      text:
+        message.text.length > maxMessageSize
+          ? message.text.substring(0, maxMessageSize) + "..."
+          : message.text,
+      created: message.created.toLocaleString(language),
+    }));
+
   res.render("index", {
     content: "messages-list",
-    messages: messages,
+    messages: processedMessages,
   });
 }
 
